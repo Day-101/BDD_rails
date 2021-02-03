d469a819e9bf74fd3e1947c22258f9162251796a
HumanBojack <spychalar59@gmail.com>
2021-02-03 16:04:04 +0100

    Chinook music exercise

--- allocine/db/seeds.rb
+++ allocine/db/seeds.rb
@@ -1,4 +1,3 @@
-Movie.all.destroy_all
 100.times do |m|
   Movie.create(name: Faker::Movie.unique.title, year: rand(1900..2020) , genre: ["action", "horreur", "comédie", "drame"].sample , synopsis: Faker::Movie.quote , director: Faker::Name.name , allocine_rating: rand(0.0..5.0).round(1) , my_rating: nil , already_seen: false)
 end
